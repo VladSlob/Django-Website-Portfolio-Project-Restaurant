45f70637bede9adb5b715c83c1c7618f81f916ea
VladSlob <141657806+VladSlob@users.noreply.github.com>
2025-05-06 09:05:19 +0300

    test_views.py

---
+++ kitchen_services/tests/test_views.py
@@ -0,0 +1,58 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+
+from kitchen_services.models import DishType
+
+DISH_TYPE_URL = reverse("kitchen_services:dish-type-list")
+
+class PublicDishTypeTests(TestCase):
+    def test_login_required(self):
+        res = self.client.get(DISH_TYPE_URL)
+        self.assertNotEqual(res.status_code, 200)
+
+
+class PrivateDishTypeTests(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username="test",
+            password="test123",
+        )
+        self.client.force_login(self.user)
+
+    def test_retrieve_dish_types(self):
+        DishType.objects.create(name="Dessert")
+        DishType.objects.create(name="Main dish")
+        response = self.client.get(DISH_TYPE_URL)
+        self.assertEqual(response.status_code, 200)
+        dish_types = DishType.objects.all()
+        self.assertEqual(
+            list(response.context["dish_type_list"]),
+            list(dish_types)
+        )
+        self.assertTemplateUsed(response, "kitchen_services/dish_type_list.html")
+
+
+class PrivateCookTests(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username="test",
+            password="password123",
+        )
+        self.client.force_login(self.user)
+
+    def test_create_cook(self):
+        form_data = {
+            "username": "new_user",
+            "password1": "user12test",
+            "password2": "user12test",
+            "first_name": "Test first",
+            "last_name": "Test last",
+            "years_of_experience": 20,
+        }
+        self.client.post(reverse("kitchen_services:cook-create"), data=form_data)
+        new_user = get_user_model().objects.get(username=form_data["username"])
+
+        self.assertEqual(new_user.first_name, form_data["first_name"])
+        self.assertEqual(new_user.last_name, form_data["last_name"])
+        self.assertEqual(new_user.years_of_experience, form_data["years_of_experience"])
